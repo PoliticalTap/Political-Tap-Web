a353dfa5b62948624abbf0a5a780b3a9cd66c36d
chaklahk <chaklahk@mail.uc.edu>
2021-03-15 19:34:48 -0400

    Update HTTPS headers

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const app = express();
 const forceSsl = require('force-ssl-heroku');
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Political-Tap-Window'));
 app.use(forceSsl);
+app.use(express.static('./dist/Political-Tap-Window'));
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+      next();
+    }
+  });
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/Political-Tap-Window/'}),
