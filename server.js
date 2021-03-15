c16d0f9d6ce6f0b46d874f77729660719e781d25
chaklahk <chaklahk@mail.uc.edu>
2021-03-15 19:12:20 -0400

    Force HTTPS

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const forceSsl = require('force-ssl-heroku');
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/Political-Tap-Window'));
+app.use(forceSsl);
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/Political-Tap-Window/'}),
