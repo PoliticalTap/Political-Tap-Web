c77b31f52a60cc60dd7f14607ff131536afe7e6c
chaklakl <chaklakl@mail.uc.edu>
2021-03-19 14:34:53 -0400

    Location Form to Officials

--- src/app/officials/officials.component.ts
+++ src/app/officials/officials.component.ts
@@ -8,14 +8,55 @@ import { CandidateService } from '../candidate.service';
 })
 
 export class OfficialsComponent implements OnInit {
+  zip: string = sessionStorage.getItem('zip') ?? "Unknown";
+
   officials: any;
   noZip = true;
   loading = false;
 
   constructor(private candidateService: CandidateService) { }
 
-  ngOnInit(): void {
-    this.getOfficials();
+  async ngOnInit() {
+    try {
+      if (this.zip == "Unknown") {
+        var coords = await this.getDeviceLocation();
+        this.zip = await this.getZipFromCoords(coords.coords.latitude.toString(), coords.coords.longitude.toString());
+
+        sessionStorage.setItem('zip', this.zip);
+      } else {
+
+      }
+
+      this.officials = await this.getOfficials();
+      this.loading = false;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getDeviceLocation(): Promise<GeolocationPosition> {
+    return new Promise<GeolocationPosition>((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
+
+  getZipFromCoords(lat, long) {
+    return this.candidateService.getUserZipFromCoords(lat, long).toPromise();
+  }
+
+  async onUpdateZipThroughDeviceLocation() {
+    try {
+      var userPosition = await this.getDeviceLocation();
+      this.zip = await this.getZipFromCoords(userPosition.coords.latitude.toString(), userPosition.coords.longitude.toString());
+      sessionStorage.setItem('zip', this.zip);
+
+      this.officials = await this.getOfficials();
+      this.loading = false;
+
+    } catch(error) {
+      console.log(error);
+    }
   }
   
   toggleAccordian(event, index) {
@@ -28,7 +69,7 @@ export class OfficialsComponent implements OnInit {
       panel.hidden = !panel.hidden;
   }
 
-  getOfficials() {
+  async getOfficials() {
     var zip = sessionStorage.getItem('zip');
     
     if(zip)
@@ -37,14 +78,17 @@ export class OfficialsComponent implements OnInit {
       this.loading = true;
     }
 
-    this.candidateService.getOfficialsList(sessionStorage.getItem('zip'))
-      .subscribe(officials => 
-      {
-        this.officials = officials;
-        this.loading = false;
-        console.log(this.officials);
-      }
-    );
+    return this.candidateService.getOfficialsList(zip).toPromise();
+  }
+
+  async submitNewZip(form) {
+    if (form.value.newZip != "") {
+      this.zip = form.value.newZip;
+      sessionStorage.setItem('zip', this.zip);
+
+      this.officials = await this.getOfficials();
+      this.loading = false;
+    }
   }
 
 }
